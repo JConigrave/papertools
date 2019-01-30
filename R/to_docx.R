d01e90b4516168e21dc5381b558ee9d062d45f69
JConigrave <james.conigrave@gmail.com>
2019-01-30 11:10:14 +1100

    added zotero notes fn

--- R/to_docx.R
+++ R/to_docx.R
@@ -1,3 +1,11 @@
+file.opened <- function(path) {
+  suppressWarnings("try-error" %in% class(try({
+    x = file(path,
+             open = "w")
+    close.connection(x)
+  }, silent = T)))
+}
+
 #'to_docx
 #'
 #'Sends a data.frame to a word doc.
@@ -7,15 +15,6 @@
 
 
 to_docx = function(table, path, table_name = NULL) {
-
-  file.opened <- function(path) {
-    suppressWarnings("try-error" %in% class(try({
-      x = file(path,
-               open = "w")
-      close.connection(x)
-    }, silent = T)))
-  }
-
   file_name = basename(path)
   dir_name = dirname(path)
   file_path = paste0(dir_name, "/", file_name)
@@ -44,3 +43,57 @@ if(!"list" %in% class(table)){
                     quiet = T)
   message(paste0("saved to: '", dir_name, "/", file_name, "'"))
 }
+
+#'zotero_notes
+#'
+#'Sends a data.frame to a word doc.
+#'@param csv an exported csv from zotero. Study type stored in the extra field
+#'@param path a string. must end in .html
+#'@param title a string. Defaults to "Zotero notes"
+#'@param date a string. Defaults to current date
+#'@importFrom magrittr %>%
+#'@importFrom dplyr select mutate arrange
+#'@export zotero_notes
+
+zotero_notes = function(csv, path, title = "Zotero notes", date = format(Sys.time(), '%d %B, %Y')){
+  x = read.csv(csv)
+  x$Author = gsub(",.*",", et al.",x$Author)
+  x$Notes = as.character(x$Notes)
+  x$Notes[is.na(x$Notes)] = "   "
+
+  file_name = basename(path)
+  dir_name = dirname(path)
+  file_path = paste0(dir_name, "/", file_name)
+  if (dir_name == ".") {
+    dir_name = getwd()
+  }
+
+  if (file.opened(file_path)) {
+    stop("Target file is currently open, cannot write.")
+  }
+
+  rmarkdownpath = system.file("rmd", "zotero_notes.Rmd", package = "papertools")
+
+  final_table = x %>%
+    dplyr::select(
+      Author,
+      Year = "Publication.Year",
+      Title,
+      Journal = "Publication.Title",
+      Type = Extra,
+      Notes
+    ) %>%
+    dplyr::mutate(Type = tolower(Type)) %>%
+    dplyr::arrange(desc(Year), Author)
+
+  rmarkdown::render(rmarkdownpath,
+                    output_file = file_name,
+                    output_dir = dir_name,
+                    quiet = T)
+  message(paste0("saved to: '", dir_name, "/", file_name, "'"))
+
+
+}
+
+
+
